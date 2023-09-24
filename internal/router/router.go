17db5bbd80f357d240bef740ea883bdb1d5d1cf8
David <davka8989@gmail.com>
2023-09-24 15:10:54 +0300

    finished iter8

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,12 +2,14 @@ package router
 
 import (
 	"github.com/DavidGQK/go-link-shortener/internal/logger"
+	"github.com/DavidGQK/go-link-shortener/internal/middleware/gzip"
 	"github.com/DavidGQK/go-link-shortener/internal/server"
 	"github.com/go-chi/chi/v5"
 )
 
 func NewRouter(s server.Server) chi.Router {
 	r := chi.NewRouter()
+	r.Use(gzip.Middleware)
 	r.Get("/{id}", logger.WithLogging(s.GetContent))
 	r.Post("/", logger.WithLogging(s.PostShortenLink))
 	r.Post("/api/shorten", logger.WithLogging(s.PostAPIShortenLink))
